16a0b097f9cba7f5abc3757117c6c22237e8c8f0
iurii.vatazhyshyn <iurii.vatazhyshyn@evinced.com>
2023-04-03 16:37:21 +0300

    removed whitelist

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,8 +1,6 @@
-const allowedOrigins = require('./allowedOrigins');
-
 const corsOptions = {
     origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        if (!origin) {
             callback(null, true);
         } else {
             callback(new Error('Not allowed by CORS'));
